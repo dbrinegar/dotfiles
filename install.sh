d995dca7824c6ae1ff8699a8ac55aef2b80881c5
David Brinegar <david.brinegar@me.com>
2015-03-05 10:02:19 -0800

    install brew basics if needed

--- install.sh
+++ install.sh
@@ -1,3 +1,50 @@
 #!/bin/sh
 
+IFS='
+'
+
+# install brew if needed
+
+which -s brew || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# brew taps
+
+tapifneeded() {
+  brew tap | grep -qx "$1" || brew tap "$1"
+}
+
+taps="
+dbrinegar/dbrinegar
+homebrew/science
+"
+
+for tap in $taps
+do
+  tapifneeded "$tap"
+done
+
+# essential brew packages
+
+brewifneeded() {
+  brew list "$1" 2>/dev/null >/dev/null || brew install "$1"
+}
+
+brews="
+bash-completion
+ctags
+dbrinegar/dbrinegar/gitutils
+dnsmasq
+jq
+node
+nvm
+parallel
+shellcheck
+"
+
+for b in $brews
+do
+  brewifneeded "$b"
+done
+
+# install home files
 
